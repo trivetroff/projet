6556b83f5efea5e8901786f4bbadf3e920c6810b
tristan <tristan@vetroff.fr>
2018-03-17 04:49:40 -0400

    modify town.rb

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,2 +1,13 @@
 class Town < ActiveRecord::Base
+  before_validation :load_position
+  validates :latitude, :longitude, presence: true
+  
+  private
+  def load_position
+    places = Nominatim.search(town_name).limit(1).first
+    if places
+      self.latitude = places.lat
+      self.longitude = places.lon
+    end
+  end
 end
