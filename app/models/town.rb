7174f2f8ce00503bb061d7b03e1c57db913e342c
tristan <tristan@vetroff.fr>
2018-03-17 06:09:39 -0400

    modify something

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,13 +1,13 @@
 class Town < ActiveRecord::Base
   before_validation :load_position
-  validates :latitude, :longitude, presence: true
+  validates :latitude, :long, presence: true
   
   private
   def load_position
-    places = Nominatim.search(town_name).limit(1).first
+    places = Nominatim.search(name).limit(1).first
     if places
       self.latitude = places.lat
-      self.longitude = places.lon
+      self.long = places.lon
     end
   end
 end
--- test/models/town_test.rb
+++ test/models/town_test.rb
@@ -4,4 +4,10 @@ class TownTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "town validation does geocoding" do
+    belfort = Town.new
+    belfort.name ='belfort'
+    assert belfort.valid?
+    assert_equal(47.6379599, belfort.latitude)
+  end
 end
