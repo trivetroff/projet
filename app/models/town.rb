2d7cc5caba7b571b4e05aa7758c6a7edc0198d8b
tristan <tristan@vetroff.fr>
2018-03-22 05:54:52 -0400

    add weather

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,13 +1,25 @@
 class Town < ActiveRecord::Base
-  before_validation :load_position
-  validates :latitude, :long, presence: true
-  
+    validates :name, :postal_code, presence: true
+    before_validation :getLocation
+
+  public
+  def get_weather
+    if self.lat && self.long
+      forecast = ForecastIO.forecast(self.lat,self.long, params: { units:'si', lang: 'fr' })
+      if forecast
+        return forecast.currently
+      end
+    end
+    return nil
+  end
+
   private
-  def load_position
-    places = Nominatim.search(name).limit(1).first
-    if places
-      self.latitude = places.lat
-      self.long = places.lon
+  def getLocation
+    city = Nominatim.search(self.name).limit(1).first
+    if city
+      self.lat = city.lat
+      self.long = city.lon
     end
   end
+
 end
---
+++ config/initializers/initforecast.rb
@@ -0,0 +1,3 @@
+ForecastIO.configure do |c|
+  c.api_key = 574c93bf03341765cd8499c6bc433125
+end
